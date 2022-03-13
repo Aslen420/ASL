2dd27fe59eb6c4b5f3c9ed7f3c03177b89964b91
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-13 00:25:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <cstring>
+#include "src/toCelsius.hpp"
+#include "src/toFahrenheit.hpp"
 
-int main() {
-  
+int main(int argc, char* argv[]) {
+    if (strcmp(argv[1], "help")==0)
+    {
+        std::cout << "fh - Celsius to Fahrenheit Converter" << std::endl;
+        std::cout << "ch - Fahrenheit to Celsius Converter" << std::endl;
+    }
+    else if (strcmp(argv[1], "fh")==0)
+    {
+        toFahrenheit();
+    }
+    else if (strcmp(argv[1], "ch")==0) {
+        toCelsius();
+    }
+    else {
+        std::cout << argv[0] << argv[1];
+    }
 }
