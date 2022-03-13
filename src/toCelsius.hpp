38935e9a3af073d83a7d6fa0e202f897716342ff
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-12 23:57:17 -0500

    Update toCelsius.hpp

--- src/toCelsius.hpp
+++ src/toCelsius.hpp
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+	printf("Enter a number to convert to celsius : ");
+	double num1;
+	scanf("%lf", &num1);
+	num1 = num1 - 32;
+	num1 = num1 * 5;
+	num1 = num1 / 9;
+	printf("%.2lf\n", num1);
+}
