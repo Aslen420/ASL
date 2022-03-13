a25f73f3d23fba2210e9b54bcba1fea52e380dde
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-12 23:57:36 -0500

    Create toFahrenheit.cpp

---
+++ src/toFahrenheit.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("Enter a number to convert to fahrenheit : ");
+	double num1;
+	scanf("%lf", &num1);
+	num1 = (num1 * 1.8) + 32;
+	printf("%.2lf\n", num1);
+}
