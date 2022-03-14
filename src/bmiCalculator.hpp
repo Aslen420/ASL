cec25c1d104fe3bd07d33b5554b5018f1172b11e
Joshubruh <101490794+Joshubruh@users.noreply.github.com>
2022-03-14 18:23:36 -0400

    Update bmiCalculator.hpp

--- src/bmiCalculator.hpp
+++ src/bmiCalculator.hpp
@@ -6,46 +6,49 @@
 void BMI() {
 
     char units;
-    double metricHeight, imperialHeight, bmi, lbs, kgs;
+    double metricHeight, imperialHeightFt, imperialHeightIn, bmi, lbs, kgs;
 
     std::cout << "Imperial or metric [first char, lowercase]: ";
     std::cin >> units;
     
     switch (units) {
         case 'i':
+        std::cout << "Enter your height in feet: ";
+        std::cin >> imperialHeightFt;
         std::cout << "\nEnter your height in inches: ";
-        std::cin >> imperialHeight;
+        std::cin >> imperialHeightIn;
         std::cout << "\nEnter your weight in lbs: ";
         std::cin >> lbs;
 
-        bmi = lbs / (imperialHeight * imperialHeight) * 703;
-        std::cout << "Your BMI is: " << bmi;
+        imperialHeightIn += imperialHeightFt * 12;
+
+        bmi = lbs / (imperialHeightIn * imperialHeightIn) * 703;
+        std::cout << "\nYour BMI is: " << bmi;
         break;
 
         case 'm':
-        std::cout << "\n\nWhat is your height in centimeters?: ";
+        std::cout << "\nWhat is your height in centimeters?: ";
         std::cin >> metricHeight;
-        std::cout << "\n\nWhat is your weight in kilograms?: ";
+        std::cout << "\nWhat is your weight in kilograms?: ";
         std::cin >> kgs;
 
         bmi = (kgs / metricHeight / metricHeight) * 10000;
-        std::cout << "\n\nYour BMI is: " << bmi << "\n\n";
+        std::cout << "\nYour BMI is: " << bmi;
         break;
     }
 
     if(bmi < 24) {
-        std::cout << "\n\nYou are underweight\n\n";
+        std::cout << "\nYou are underweight\n";
     } else if(bmi < 30 && bmi > 24) {
-        std::cout << "\n\nYou are normal weight\n\n";
+        std::cout << "\nYou are normal weight\n";
     } else if(bmi > 30 && bmi < 35) {
-        std::cout << "\n\nYou are overweight\n\n";
+        std::cout << "\nYou are overweight\n";
     } else if(bmi > 35){
-        std::cout << "\n\nYou are morbidly obese\n\n";
+        std::cout << "\nYou are morbidly obese\n";
     } else {
-        std::cout << "\n\nEnter a valid value\n\n";
+        std::cout << "\nEnter a valid value\n";
     }
 }
-
 #endif
     
         
