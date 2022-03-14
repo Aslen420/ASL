2a7b6949e30938b8f4bda65f8c02dfea7ca8fe08
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-13 20:40:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ mkdir ASL
 cd ASL
 git clone https://github.com/Aslen420/ASL
 cd ASL
-mv ASL .ASL
 g++ asl.cpp
 mv a.out asl
 cd ~
+sed -i '/asl/d' ./.bashrc
 echo 'alias asl="./ASL/ASL/asl"' >> .bashrc
 cd ~
 echo 'Complete!'
