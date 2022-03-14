4a2d0660af31923a4008a8c2d6d0aa4347778045
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-14 18:24:04 -0400

    Update asl.cpp

--- asl.cpp
+++ asl.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cstring>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #include "src/toCelsius.hpp"
 #include "src/toFahrenheit.hpp"
 #include "src/terminal-editor.hpp"
 #include "src/geoCalc.hpp"
 #include "src/bmiCalculator.hpp"
+bool EXAMPLE = true;
 
 int main(int argc, char* argv[]) {
 
@@ -23,21 +26,30 @@ int main(int argc, char* argv[]) {
     else if (strcmp(argv[1], "fh")==0)
     {
         toFahrenheit();
+        EXAMPLE = false;
     }
     else if (strcmp(argv[1], "ch")==0) {
         toCelsius();
+        EXAMPLE = false;
     }
     else if (strcmp(argv[1], "terminal-editor")==0) {
         std::cout << "Please note this feature only works on macOS and Linux systems. It is also recommended that you run this while in the home directory." << std::endl;
         termEdit();
+        EXAMPLE = false;
     }
     else if (strcmp(argv[1], "geo")==0) {
         GEO();
+        EXAMPLE = false;
     }
      else if (strcmp(argv[1], "bmi")==0) {
         BMI();
+        EXAMPLE = false;
     }
     else {
         std::cout << argv[0] << argv[1];
+    }
+    if () {
+        EXAMPLE = true; 
+        std::cout << "Incorrect usage. Use asl help for a list of commands." << std::endl;
     }
 }
