c7223d58ab847dcb9a334a272ef75374eafccdaa
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-13 21:54:47 -0400

    Update asl.cpp

--- asl.cpp
+++ asl.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[]) {
         std::cout << "fh - Celsius to Fahrenheit Converter" << std::endl;
         std::cout << "ch - Fahrenheit to Celsius Converter" << std::endl;
         std::cout << "terminal-editor - Terminal Prompt Editor" << std::endl;
+        std::cout << "geo - Geometry Calculator" << std::endl;
     }
     else if (strcmp(argv[1], "fh")==0)
     {
