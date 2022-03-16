80ddf961f6caa84246cb04b47c2f6e84f7271422
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-16 12:51:16 -0400

    Update and rename toCelsius.h to tempConv.h

--- src/tempConv.h
+++ src/tempConv.h
@@ -1,5 +1,5 @@
-#ifndef CELSIUS_H
-#define CELSIUS_H
+#ifndef TEMP_H
+#define TEMP_H
 
 #include <stdio.h>
 
@@ -12,5 +12,13 @@ void toCelsius() {
 	num1 = num1 / 9;
 	printf("%.2lf\n", num1);
 }
+void toFahrenheit() {
+	printf("Enter a number to convert to fahrenheit : ");
+	double num1;
+	scanf("%lf", &num1);
+	num1 = (num1 * 1.8) + 32;
+	printf("%.2lf\n", num1);
+}
+
 
 #endif
