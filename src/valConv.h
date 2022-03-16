28c4222a5458013d57874139bce5b0c14c4c5939
Aslen420 <99992301+Aslen420@users.noreply.github.com>
2022-03-16 14:32:27 -0400

    Create valConv.h

---
+++ src/valConv.h
@@ -0,0 +1,59 @@
+#ifndef VAL_H
+#define VAL_H
+
+#include <stdio.h>
+void val() {
+    char selec;
+    printf("Which of the following would you like to choose : \n\t A. mg > g\n\t B. g > kg\n\t C. kg > t\n\t D. g > mg\n\t E. kg > g\n\t F. t > kg\n\t > ");
+    scanf("%c", &selec);
+    if (selec == 'A' || selec == 'a') {
+        printf("Enter a value to convert > ");
+        double pre;
+        scanf("%lf", &pre);
+        pre = pre / 1000;
+        printf("%.3lf grams\n", pre);
+
+    }
+    if (selec == 'B' || selec == 'b') {
+        printf("Enter a value to convert > ");
+        double pre;
+        scanf("%lf", &pre);
+        pre = pre / 1000;
+        printf("%.3lf kilograms\n", pre);
+
+    }
+    if (selec == 'C' || selec == 'c') {
+        printf("Enter a value to convert > ");
+        double pre;
+        scanf("%lf", &pre);
+        pre = pre / 1000;
+        printf("%.3lf metric tons\n", pre);
+
+    }
+    if (selec == 'D' || selec == 'd') {
+        printf("Enter a value to convert > ");
+        double pre;
+        scanf("%lf", &pre);
+        pre = pre * 1000;
+        printf("%.1lf milligrams\n", pre);
+
+    }
+    if (selec == 'E' || selec == 'e') {
+        printf("Enter a value to convert > ");
+        double pre;
+        scanf("%lf", &pre);
+        pre = pre * 1000;
+        printf("%.1lf grams\n", pre);
+
+    }
+    if (selec == 'F' || selec == 'f') {
+        printf("Enter a value to convert > ");
+        double pre;
+        scanf("%lf", &pre);
+        pre = pre * 1000;
+        printf("%.1lf kilograms\n", pre);
+
+    }
+}
+
+#endif
